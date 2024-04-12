5b406638c9950ea8df394a03240775b63e6e8bcd
Benadicksamuvel <136051110+Benadicksamuvel@users.noreply.github.com>
2024-04-12 10:55:49 +0530

    Create jeevika.c

---
+++ jeevika.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int num, originalNum, remainder, result = 0;
+    printf("Enter a three-digit integer: ");
+    scanf("%d", &num);
+    originalNum 
+       originalNum /= 10;
+    }
+
+    if (result == num)
+        printf("%d is an Armstrong number.", num);
+    else
+        printf("%d is not an Armstrong number.", num);
+
+    return 0;
+}
